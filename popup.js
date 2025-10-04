3d16cfaf9aadf90006ba00684e6b903e22f06a3d
AndrewTruong744 <andrewtruong40024@gmail.com>
2025-10-04 12:53:29 -0500

    testing extension setup

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const button = document.getElementById('myButton');
+    
+    button.addEventListener('click', function() {
+        alert('Button clicked! You are on your way to becoming an extension developer.');
+    });
+});
